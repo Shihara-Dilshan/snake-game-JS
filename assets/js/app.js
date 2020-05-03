f5e8119fa0999882c35dc8a8f672605d02d3534c
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-05-03 08:25:00 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btn");
+  const square = document.querySelectorAll(".grid div");
+  const scoreBoard = document.querySelector("#scoreBoard");
+
+  const width = 10;
+});
